3b92a00c15983960d80af5901e2d9a896d67aec5
root <root@DESKTOP-EHL2PJ3.localdomain>
2020-11-15 12:33:34 +0100

    Third Commit

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const name = 'PEC 1!!!!';
-console.log(`Hello ${name}`)
+function VideoEnNuevaVentana (strURL, strTitulo) {
+    window.open(strURL, strTitulo, "width=560, height=415");
+}
+
+function masPeliculas(event) {
+    window.location=this.options[this.selectedIndex].value;
+}
