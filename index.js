c163453f98d5d30a691ba96d02d77843122972c5
root <root@DESKTOP-EHL2PJ3.localdomain>
2020-11-15 13:03:07 +0100

    Fourth Commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function VideoEnNuevaVentana (strURL, strTitulo) {
     window.open(strURL, strTitulo, "width=560, height=415");
 }
 
-function masPeliculas(event) {
-    window.location=this.options[this.selectedIndex].value;
-}
+function masPeliculas() {
+    var x = document.getElementById("otraPeliculas").value;
+    window.location=x;
+  }
